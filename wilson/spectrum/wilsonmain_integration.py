f69f8df6962b829a21257ee1080bf1776d966102
ringholm <mringholm@gmail.com>
2025-05-23 16:07:24 +0200

    Add wilson main receiver function skeletons

--- wilson/spectrum/wilsonmain_integration.py
+++ wilson/spectrum/wilsonmain_integration.py
@@ -1,2 +1,21 @@
 from spectrum2D import Spectrum2D, numcombperm
 
+def get_spectrum2D(system, experiment, terms, props, spec_eval_setup, vib_ana_setup, with_diagnostics=False):
+
+    if not(with_diagnostics):
+        return []
+
+    else:
+        return [], []
+
+def spectrum2D_with_diagnostics(system, experiment, terms, props, spec_eval_setup, vib_ana_setup):
+
+    return get_spectrum2D(system, experiment, terms, props, spec_eval_setup, vib_ana_setup, with_diagnostics=True)
+
+
+def spectrum2D(system, experiment, terms, props, spec_eval_setup, vib_ana_setup):
+
+    return get_spectrum2D(system, experiment, terms, props, spec_eval_setup, vib_ana_setup, with_diagnostics=True)
+
+
+
