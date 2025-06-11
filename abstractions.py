dab20258bbc98a1e4959b564e0d17e38a502f2d1
ringholm <mringholm@gmail.com>
2025-06-11 16:30:06 +0200

    Baby steps on common classes, only small notes for now

---
+++ abstractions.py
@@ -0,0 +1,42 @@
+# Drafting candidate common classes
+#
+# TODO: Use class inheritance? Classes here may have a self-contained analytical/symbolic aspect and additionally a numerical aspect
+
+# Vibrational state:
+# Note: Some attributes I'm not sure whether to consider as symbolic or numerical
+# Suggested symbolic attributes
+# 	- Composition/coefficients (symbolic): Unsure about use, for derivation work maybe consider always composite (since non-composite is a special case with one coeff = 1)
+# 
+# 
+# Suggested numerical attributes
+# 
+# 
+# 
+#
+# Suggested methods
+# 
+class vibState:
+
+
+	def __init__(self):
+	
+		pass
+		
+		
+class vibAna:
+
+
+	def __init__(self):
+	
+		pass		
+
+		
+class spectroTerm:
+
+
+	def __init__(self):
+	
+		pass		
+		
+		
+
