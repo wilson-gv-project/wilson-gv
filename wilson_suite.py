6722ceb086a0dd03193a74a2f76e5a3b04aacaef
wlevand <95315734+wlevand@users.noreply.github.com>
2025-06-11 22:34:59 +0200

    files for suite installation

---
+++ wilson_suite.py
@@ -0,0 +1,6 @@
+import wilson_analysis as wilson_analysis
+import wilson_main as wilson_main
+import wilson_derive as wilson_derive
+import wilson_experiment as wilson_experiment
+import wilson_utils as wilson_utils
+import wilson as wilson_intensities
