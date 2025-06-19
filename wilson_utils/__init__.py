d0e700fd4443855950028c53db92d2fc7cd3c0f0
ringholm <mringholm@gmail.com>
2025-06-19 16:42:57 +0200

    Updating init and adding trivname to abstractions, rm former sketch of classes

---
+++ wilson_utils/__init__.py
@@ -0,0 +1,1 @@
+from wilson_utils import abstractions
--- wilson_utils/abstractions.py
+++ wilson_utils/abstractions.py
@@ -1,42 +1,26 @@
-# Drafting candidate common classes
-#
-# TODO: Use class inheritance? Classes here may have a self-contained analytical/symbolic aspect and additionally a numerical aspect
-
-# Vibrational state:
-# Note: Some attributes I'm not sure whether to consider as symbolic or numerical
-# Suggested symbolic attributes
-# 	- Composition/coefficients (symbolic): Unsure about use, for derivation work maybe consider always composite (since non-composite is a special case with one coeff = 1)
-# 
-# 
-# Suggested numerical attributes
-# 
-# 
-# 
-#
-# Suggested methods
-# 
-class vibState:
 
 
-	def __init__(self):
-	
-		pass
-		
-		
-class vibAna:
 
+def prop_trivname(ord_geo=0, ord_el=0, ord_rot=0):
 
-	def __init__(self):
-	
-		pass		
+	triv_el = {0: '', 1: 'dip', 2: 'pol', 3: 'hyp', 4: 'shyp', 5: 'thyp', 6: '4hyp'}
+	triv_geo = {0: '', 1: 'grad', 2: 'hess', 3: 'cff', 4: 'qff', 5: '5ff', 6: '6ff'}
 
-		
-class spectroTerm:
+	if (ord_el == 0) and (ord_geo == 0) and (ord_rot == 0):
+		return 'E'
 
+	if ord_rot > 0:
 
-	def __init__(self):
-	
-		pass		
-		
-		
+		if (ord_rot == 1) and (ord_geo == 0):
+			return 'B'
 
+		elif (ord_rot == 1) and (ord_geo == 2):
+			return 'coriolis'
+
+		else:
+			raise AssertionError('This property trivial name is not established in prop_trivname')
+
+	if (ord_el > 6) or (ord_geo > 6):
+			raise AssertionError('This property trivial name is not established in prop_trivname')
+
+	return triv_el[ord_el] + triv_geo[ord_geo]
