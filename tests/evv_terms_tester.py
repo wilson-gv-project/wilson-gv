f9a2716246b0d8746e6f5b2a22dbe04d44eb2e06
ringholm <mringholm@gmail.com>
2025-07-14 13:28:04 +0200

    Update tests wrt updates to main

--- tests/evv_terms_tester.py
+++ tests/evv_terms_tester.py
@@ -1,20 +1,20 @@
 import wilson_suite as ws
 
-pulse_ir_1 = ws.experiment.abstractions.emPulse('ideal', 1.0e-5, tc=50.0, cf=0.00, wv=[0.0, 0.0, 1.0],
+pulse_ir_1 = ws.experiment.abstractions.EmPulse('ideal', 1.0e-5, tc=50.0, cf=0.00, wv=[0.0, 0.0, 1.0],
                                                 pol=[0.0, 0.0, 1.0], id=1)
-pulse_ir_2 = ws.experiment.abstractions.emPulse('impulsive', 1.0e-5, tc=100.0, cf=None, wv=[0.0, 0.0, 1.0],
+pulse_ir_2 = ws.experiment.abstractions.EmPulse('impulsive', 1.0e-5, tc=100.0, cf=None, wv=[0.0, 0.0, 1.0],
                                                 pol=[0.0, 0.0, 1.0], id=2)
-pulse_uvvis_1 = ws.experiment.abstractions.emPulse('ideal', 1.0e-5, tc=120.0, cf=0.0, cf_uv=0.072, wv=[0.0, 0.0, 1.0],
+pulse_uvvis_1 = ws.experiment.abstractions.EmPulse('ideal', 1.0e-5, tc=120.0, cf=0.0, cf_uv=0.072, wv=[0.0, 0.0, 1.0],
                                                    pol=[0.0, 0.0, 1.0], id=3)
 
 pulses = [pulse_ir_1, pulse_ir_2, pulse_uvvis_1]
 
-field_a = ws.experiment.abstractions.electricField(pulses)
+field_a = ws.experiment.abstractions.ElectricField(pulses)
 order = len(pulses)
 
 epochs = field_a.findEpochs()
 
-detector_a = ws.experiment.abstractions.specDetector('freq', detector_location=[0.0, 0.0, 1.0],
+detector_a = ws.experiment.abstractions.SpecDetector('freq', detector_location=[0.0, 0.0, 1.0],
                                                      detection_polarization=[0.0, 0.0, 1.0],
                                                      detection_range=[0.003 + 0.0001 * i for i in range(101)],
                                                      wv_filter=[
@@ -23,9 +23,9 @@ detector_a = ws.experiment.abstractions.specDetector('freq', detector_location=[
 # Push one carrier freq
 scan_obj_a = [['pulse', 1, 'cf', 1.0], ['detector', 0, 'detection_range', 1.0]]
 scan_range_a = [0.0001 * i for i in range(101)]
-scan_a = ws.experiment.abstractions.specScan(scan_obj_a, scan_range_a)
+scan_a = ws.experiment.abstractions.SpecScan(scan_obj_a, scan_range_a)
 
-experiment_a = ws.experiment.abstractions.vibExperiment(order, field_a, detector_a, [scan_a], magn_conditions=[[-1, 2]])
+experiment_a = ws.experiment.abstractions.VibExperiment(order, field_a, detector_a, [scan_a], magn_conditions=[[-1, 2]])
 
 fully_enhanced_terms = ws.derive.main.get_fully_enhanced_terms(experiment_a)
 
@@ -53,7 +53,7 @@ for i in fully_enhanced_terms:
 			print(' ')
 
 		else:
-			# fully_enhanced_terms[i][j] is a list! of wilson_derive.abstractions.vibPerturbedTerm objects
+			# fully_enhanced_terms[i][j] is a list! of wilson_derive.abstractions.VibPerturbedTerm objects
 			for k in fully_enhanced_terms[i][j]:
 				print('\n  --> k in fully_enhanced_terms[i][j]: k is', k)
 				print('  k is an element of', type(fully_enhanced_terms[i][j]))
--- tests/evv_tester.py
+++ tests/evv_tester.py
@@ -18,18 +18,18 @@ evaluator(self.system, self.exp, self.terms, self.props, self.spec_eval_setup, s
 """
 import wilson_suite as ws
 
-pulse_ir_1 = ws.experiment.abstractions.emPulse('ideal', 1.0e-5, tc = 50.0, cf=0.00, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=1)
-pulse_ir_2 = ws.experiment.abstractions.emPulse('impulsive', 1.0e-5, tc = 100.0, cf=None, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=2)
-pulse_uvvis_1 = ws.experiment.abstractions.emPulse('ideal', 1.0e-5, tc = 120.0, cf=0.0, cf_uv=0.072, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=3)
+pulse_ir_1 = ws.experiment.abstractions.EmPulse('ideal', 1.0e-5, tc = 50.0, cf=0.00, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=1)
+pulse_ir_2 = ws.experiment.abstractions.EmPulse('impulsive', 1.0e-5, tc = 100.0, cf=None, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=2)
+pulse_uvvis_1 = ws.experiment.abstractions.EmPulse('ideal', 1.0e-5, tc = 120.0, cf=0.0, cf_uv=0.072, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=3)
 
 pulses = [pulse_ir_1, pulse_ir_2, pulse_uvvis_1]
 
-field_a = ws.experiment.abstractions.electricField(pulses)
+field_a = ws.experiment.abstractions.ElectricField(pulses)
 order = len(pulses)
 
 epochs = field_a.findEpochs()
 
-detector_a = ws.experiment.abstractions.specDetector('freq', detector_location=[0.0, 0.0, 1.0],
+detector_a = ws.experiment.abstractions.SpecDetector('freq', detector_location=[0.0, 0.0, 1.0],
                                                      detection_polarization=[0.0, 0.0, 1.0],
                                                      detection_range=[0.003 + 0.0001*i for i in range(101)],
                                                      wv_filter=[{1: [-1], 2: [1], 3: [1]}]) #, {1: [-1], 2: [1], 3: [1]}
@@ -37,35 +37,35 @@ detector_a = ws.experiment.abstractions.specDetector('freq', detector_location=[
 # Push one carrier freq
 scan_obj_a = [['pulse', 1, 'cf', 1.0], ['detector', 0, 'detection_range', 1.0]]
 scan_range_a = [0.0001*i for i in range(101)]
-scan_a = ws.experiment.abstractions.specScan(scan_obj_a, scan_range_a)
+scan_a = ws.experiment.abstractions.SpecScan(scan_obj_a, scan_range_a)
 
-experiment_a = ws.experiment.abstractions.vibExperiment(order, field_a, detector_a, [scan_a], magn_conditions=[[-1, 2]])
+experiment_a = ws.experiment.abstractions.VibExperiment(order, field_a, detector_a, [scan_a], magn_conditions=[[-1, 2]])
 
 
-calc_setup = ws.main.abstractions.externalCalcSetup(program='gaussian', lvl_theory='B3LYP', basis='cc_pVQZ')
+calc_setup = ws.main.abstractions.ExternalCalcSetup(program='gaussian', lvl_theory='B3LYP', basis='cc_pVQZ')
 
-sim = ws.main.abstractions.wilsonSimulation()
+sim = ws.main.abstractions.WilsonSimulation()
 
 sim.addExperiment(experiment_a)
 sim.getTerms(ws.derive.main.get_fully_enhanced_terms) # here terms are derived
-mol_system = ws.main.abstractions.molecularSystem(name='ACAC', natoms=8)
+mol_system = ws.main.abstractions.MolecularSystem(name='ACAC', natoms=8)
 sim.addSystem(mol_system)
 # sim.addVibAnaSetup(ws.main.abstractions.vibAnaSetup(vib_regime='GVPT2', vibana_prop_need='anharm',
 #                                                     allow_skip_eigvec=True, external_fill_from=calc_setup))
-sim.addVibAnaSetup(ws.main.abstractions.vibAnaSetup(system=mol_system, vib_regime='GVPT2', vibana_prop_need='none',
+sim.addVibAnaSetup(ws.main.abstractions.VibAnaSetup(system=mol_system, vib_regime='GVPT2', vibana_prop_need='none',
                                                     allow_skip_eigvec=True, external_fill_from=calc_setup))
 sim.addPropEvalSetup(eval_uniform=calc_setup)
 
-axis1 = ws.main.abstractions.spectralAxis({1: 1})
-axis2 = ws.main.abstractions.spectralAxis({1: 1, 2: -1})
+axis1 = ws.main.abstractions.SpectralAxis({1: 1})
+axis2 = ws.main.abstractions.SpectralAxis({1: 1, 2: -1})
 start = {1: 250, 2: 100}
 end = {1: 3850, 2: 7550}
 spacer = {1: 3.8, 2: 3.8}
-spec_grid = ws.main.abstractions.spectralGrid({1: axis1, 2: axis2}, range_style='uniform',
+spec_grid = ws.main.abstractions.SpectralGrid({1: axis1, 2: axis2}, range_style='uniform',
                                               start=start, end=end, spacer=spacer)
 evi = {'dynrange': 500, 'Gamma': 4.7, 'diag_margin': 5., 'maxmax': None}
 rndi = {'num_level_ticks': 15}
-eval_setup = ws.main.abstractions.specEvalSetup(grid=spec_grid, ev_info=evi, rnd_info=rndi)
+eval_setup = ws.main.abstractions.SpecEvalSetup(grid=spec_grid, ev_info=evi, rnd_info=rndi)
 sim.addSpecEvalSetup(eval_setup)
 
 sim.findPropsAndMaxStateLvl() # setting up self.props/sim.props
