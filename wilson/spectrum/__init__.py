bc19dd6e8c8a003f17730ddbbeff5d4492ff3556
Vanda Le <vandwlev@gmail.com>
2025-07-16 14:35:06 +0200

    rm hidden file

--- wilson/spectrum/__init__.py
+++ wilson/spectrum/__init__.py
@@ -2,9 +2,6 @@ from .averaging import get_iso_f, get_AlphaBetaGammaDelta_indices
 from .vpt2 import anharm_corr_energies, get_X
 from ..utils.tools import convNu2Ene, match_modes
 
-from .hidden_cake_amplitudes import (FactorTensor, ComponentsLayer,
-                                     combine_into_cake, combine_into_layer, sum_cake, get_slice)
-
 from .termsEvaluator import *
 from ..utils.spectrum_utils import *
 
