a0e5173a53a9d54ba08210c13b696bb0eac521ed
Vanda Le <vandwlev@gmail.com>
2025-07-22 01:35:40 +0200

    made dataclasses and to_dict methods

--- wilson_experiment/abstractions.py
+++ wilson_experiment/abstractions.py
@@ -4,7 +4,26 @@ import copy
 # TODO: SpecDetector and SpecScan as dataclasses?
 # TODO: Expand functionality according to below TODOs
 
+from dataclasses import dataclass, field, asdict, is_dataclass
+from typing import List, Optional, Iterable
+
+@dataclass
 class SpecDetector:
+    detection_method: str
+    detector_location: Optional[List[float]] = None
+    detection_polarization: Optional[List[float]] = None
+    detection_range: Optional[List[float]] = None
+    wv_filter: Optional[List[dict]] = None
+    ignore_collinear: bool = True
+
+    def __post_init__(self):
+        if self.detection_method not in {'time', 'freq', 'int'}:
+            raise ValueError("The detection type must be either 'time', 'freq'(uency), or 'int'(egrated)")
+
+        if self.detection_range is None and (self.detection_method in ['time']):
+            raise AssertionError("The detection range must be specified when the detector is set to 'time' or 'freq' detection")
+
+class SpecDetector_reg:
     """
     Class to represent a spectral detector
     """
@@ -57,8 +76,55 @@ class SpecDetector:
 
     def __repr__(self):
         return f'THIS IS specDetector with detection_method - {self.dmethod}'
+    
+    def to_dict(self):
+        """
+        detection_method: str, detector_location: list[float]=None,
+                 detection_polarization: list[float]=None, detection_range: list[float]=None,
+                 wv_filter: list[dict]=None, ignore_collinear: bool=True
+        not always all become atributes...
+        """
+        d = {'detection_method': self.dmethod, 'detector_location': self.dloc,
+             'detection_polarization': self.dpol, 'wv_filter': self.wv_filter,
+             'ignore_collinear': self.ignore_collinear}
+        
+        if hasattr(self, 'detection_range'):
+            d['detection_range'] = self.detection_range
+        else:
+            d['detection_range'] = None
+        return d
+    
+    @classmethod
+    def from_dict(cls, data):
+        """
 
+        """
+
+        return cls(detection_method=data['detection_method'], 
+                   detector_location=data['detector_location'],
+                   detection_polarization=data['detection_polarization'], 
+                   detection_range=data['detection_range'],
+                   wv_filter=data['wv_filter'], 
+                   ignore_collinear=data['ignore_collinear'])
+
+@dataclass
 class SpecScan:
+    scan_objs: List
+    range: Iterable
+
+    def __post_init__(self):
+        valid_scan_objs = ['pulse', 'detector']
+        valid_scan_attributes = {'pulse': ['cf', 'tc', 'dev'], 'detector': ['detection_range']}
+
+        for i in self.scan_objs:
+            print('scan obj i', i)
+            if i[0] not in valid_scan_objs:
+                raise AssertionError("The scan object must be one of", valid_scan_objs, 'but is instead', i[0])
+            if i[2] not in valid_scan_attributes[i[0]]:
+                raise AssertionError("The scan attribute for", i[0], 'must be one of', valid_scan_attributes[i[0]], 'but is instead', i[2])
+
+
+class SpecScan_reg:
     """
     Class to represent a spectral scan (adding to the dimensionality of a spectrum)
     """
@@ -92,7 +158,94 @@ class SpecScan:
     def __repr__(self):
         return f'THIS IS specScan - {self.scan_objs}'
 
+    def to_dict(self):
+        """
+        __init__(self, scan_objs: list, range)
+        """
+        d = {'detection_method': self.dmethod, 'detector_location': self.dloc,
+             'detection_polarization': self.dpol, 'wv_filter': self.wv_filter,
+             'ignore_collinear': self.ignore_collinear}
+        
+        if hasattr(self, 'detection_range'):
+            d['detection_range'] = self.detection_range
+        else:
+            d['detection_range'] = None
+        return d
+
+@dataclass
 class EmPulse:
+    env: str
+    maxstr: float
+    tc: float = None
+    cf: float = None
+    cf_uv: float = 0.0
+    dev: float = None
+    wv: List[float] = None
+    pol: List[float] = None
+    id: int = None
+
+    def __post_init__(self):
+        
+        allowed_envelopes = ['impulsive', 'ideal', 'cw', 'gaussian']
+
+        if self.env not in allowed_envelopes:
+            raise AssertionError('Allowed pulse envelope choices are: "impulsive", "ideal", "cw", "gaussian"')
+
+        if not (self.cf_uv == 0.0):
+            if not (self.cf == 0.0):
+                raise AssertionError('Pulses with non-zero UV/VIS carrier freqs. must have IR carrier freq part set to zero')
+
+        if self.env == "gaussian":
+            if self.cf == None or self.dev == None:
+                raise AssertionError('A Gaussian pulse must have a carrier frequency and a deviation parameter')
+
+        if self.env == "ideal":
+            if self.cf == None:
+                raise AssertionError('An pulse of the "ideal" type must have a (monochromatic) "carrier" frequency')
+
+        # Wavevector: In which unit vector direction is the pulse wave travelling
+        if self.wv is None:
+            print('No wavevector was specified for pulse, defaulting to unit z direction wavevector')
+            self.wv = [0.0, 0.0, 1.0]
+        else:
+            if isinstance(self.wv, list):
+                if len(self.wv) == 3:
+                    if all([isinstance(i, float) for i in self.wv]):
+                        wv_len = (self.wv[0]**2.0 + self.wv[1]**2.0 + self.wv[2]**2.0)**0.5
+                        if not wv_len == 1.0:
+                            print('Wavevector was normalized')
+                        self.wv = [i/wv_len for i in self.wv]
+
+                    else:
+                        raise AssertionError('The pulse wavevector must be a len 3 list of floats')
+                else:
+                    raise AssertionError('The pulse wavevector must be a len 3 list of floats')
+            else:
+                raise AssertionError('The pulse wavevector must be a len 3 list of floats')
+
+        # Polarization: Specify the polarization of the pulse
+        # Currently supports unit linear polarization (TODO: Add support for circular polarization (as function)?)
+        if self.pol is None:
+            print('No polarization was specified for pulse, defaulting to unit z direction wavevector')
+            self.pol = [0.0, 0.0, 1.0]
+        else:
+            if isinstance(self.pol, list):
+                if len(self.pol) == 3:
+                    if all([isinstance(i, float) for i in self.pol]):
+                        pol_len = (self.pol[0]**2.0 + self.pol[1]**2.0 + self.pol[2]**2.0)**0.5
+                        if not pol_len == 1.0:
+                            print('Wavevector was normalized')
+                        self.pol = [i/pol_len for i in self.pol]
+
+                    else:
+                        raise AssertionError('The pulse wavevector must be a len 3 list of floats')
+                else:
+                    raise AssertionError('The pulse wavevector must be a len 3 list of floats')
+            else:
+                raise AssertionError('The pulse wavevector must be a len 3 list of floats')
+
+
+class EmPulse_reg:
     """
     Class to represent an electromagnetic pulse
     """
@@ -189,9 +342,82 @@ class EmPulse:
 
     def __repr__(self):
         return f'THIS IS emPulse with pulse envelope {self.env}'
+    
+    def to_dict(self):
+        d = {'env': self.env, 'tc': self.tc, 
+                'maxstr': self.maxstr, 'cf_uv': self.cf_uv, 
+                'wv': self.wv, 
+                'pol': self.pol, 'id': self.id}
+        if hasattr(self, "cf"):
+            d['cf'] = self.cf
+        return d
+    
+    @classmethod
+    def from_dict(cls, data):
+        """
+
+        """
+        if 'cf' in data:
+            cf = data['cf']
+        else:
+            cf = None
+        if 'dev' in data:
+            dev = data['dev']
+        else:
+            dev = None
+        return cls(env=data['env'], maxstr=data['maxstr'], 
+                   tc=data['tc'], cf=cf, cf_uv=data['cf_uv'], dev=dev,
+                   wv=data['wv'], pol=data['pol'], id=data['id'])
+
+@dataclass
+class ElectricField:
+    pulses: List[EmPulse]
+
+    def findEpochs(self, tol: float=0.0) -> list:
+        """
+        Divide field into epochs either with zero or finite tolerance
+        Currently only supported for a field consisting of ideal or impulsive pulses
+
+        tol: Float: Tolerance for non-temporal coincidence (currently not supported)
+        # TODO: Add support for tolerance
+
+        Returns: List of lists: [[epoch 1 pulse 1, epoch 1 pulse 2, ...], [epoch 2 pulse 1, ...], ...]
+        """
+
+        for i in self.pulses:
+            if i.env not in ['ideal', 'impulsive']:
+                raise AssertionError('Can currently only determine epochs for fields consisting of only ideal or impulsive pulses')
+            if i.id is None:
+                raise AssertionError('All pulses must have IDs for valid epoch determination')
+
+        times_ids = sorted([(i.tc, i.id) for i in self.pulses], key=itemgetter(0))
+        epochs = [[]]
+        epoch = 0
+        curr_time = times_ids[0][0]
+
+        for i in times_ids:
+            if not(i[0] == curr_time):
+                epochs.append([])
+                epoch += 1
+                curr_time = i[0]
+            epochs[epoch].append(i[1])
+
+        return epochs
+    
+    def getCarrierFreqsUV(self) -> dict:
+        """
+        Get dictionary of UV/VIS-range part of carrier frequencies
+
+        Returns: Dictionary {pulse 1: UV/VIS carrier freq., ...}
+        """
+        cfuv_dict = {}
+        for i in self.pulses:
+            cfuv_dict[i.id] = i.cf_uv
+
+        return cfuv_dict
 
 # The field consists of a collection of pulses
-class ElectricField:
+class ElectricField_reg:
     """
     Class to represent an electromagnetic field consisting of one or more pulses
     """
@@ -246,6 +472,21 @@ class ElectricField:
             cfuv_dict[i.id] = i.cf_uv
 
         return cfuv_dict
+    
+    def to_dict(self):
+        return {'pulses': [pulse.to_dict() for pulse in self.pulses],
+                }
+    
+    @classmethod
+    def from_dict(cls, data):
+        """
+        for this:
+
+        def __init__(self, start=0):
+            self.value = start
+        """
+        pulses = [EmPulse.from_dict(pulse_dict) for pulse_dict in data['pulses']]
+        return cls(pulses = pulses)
 
 class VibExperiment:
     """
@@ -297,10 +538,12 @@ class VibExperiment:
         d = 0
         d += len(self.scans)
 
-        if self.detector.dmethod == 'time':
+        # if self.detector.dmethod == 'time':
+        if self.detector.detection_method == 'time':
             return d + 1
-
-        elif self.detector.dmethod == 'freq':
+        
+        # elif self.detector.dmethod == 'freq':
+        elif self.detector.detection_method == 'freq':
             if self.detector.detection_range is not None:
                 return d + 1
 
@@ -384,3 +627,50 @@ class VibExperiment:
         return int_sequences
 
 
+    def to_dict(self):
+        """
+        __init__
+        (self, order: int, field: ElectricField, detector: SpecDetector, scans: list[SpecScan]=None,
+                 magn_conditions: list=[])
+
+        __dict__
+        {'order': 3, 'field': <wilson_experiment.abstractions.ElectricField object at 0x7f1a27898770>, 
+        'detector': THIS IS specDetector with detection_method - freq, 
+        'scans': [THIS IS specScan - [['pulse', 1, 'cf', 1.0], ['detector', 0, 'detection_range', 1.0]]], 
+        'magn_conditions': [[-1, 2]], 'dim': 2, 
+        'epochs': [[1], [2], [3]], 
+        'int_sequences': [[{1: -1}, {2: 1}, {3: 1}]], 
+        'cfuv': {1: 0.0, 2: 0.0, 3: 0.072}}
+        """
+        attributes = ['order', 'field', 'detector', 'scans', 'magn_conditions']
+
+        result = {}
+        for k in attributes:
+            v = getattr(self, k)
+            if is_dataclass(v):
+                result[k] = asdict(v)
+            elif isinstance(v, list):
+                # are list elements dataclasses?
+                if v and all(is_dataclass(item) for item in v):
+                    result[k] = [asdict(item) for item in v]
+                else:
+                    result[k] = v
+            elif isinstance(v, tuple):
+                # are tuple elements dataclasses?
+                if v and all(is_dataclass(item) for item in v):
+                    result[k] = tuple([asdict(item) for item in v])
+                else:
+                    result[k] = v
+            else:
+                result[k] = v
+        return result
+    
+    @classmethod
+    def from_dict(cls, data):
+        """
+        for this:
+        
+        def __init__(self, start=0):
+            self.value = start
+        """
+        return cls(start=data["value"])
