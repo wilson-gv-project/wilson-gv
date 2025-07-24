3eac091488e4d06f22af781bf5a7d92683071477
Vanda Le <vandwlev@gmail.com>
2025-07-24 15:39:36 +0200

    upd script tester, made more tests from one script, testing pickling. rm data files and tester for old classes

--- tests/evv_tester_dataclasses.py
+++ tests/evv_tester_dataclasses.py
@@ -17,141 +17,138 @@ evaluator(self.system, self.exp, self.terms, self.props, self.spec_eval_setup, s
 
 """
 import wilson_suite as ws
+from wilson_utils.serialization import pickle_this_to, unpickle_smth_from
+from wilson_utils.paths import SUITE_ROOT
 
-print('\nevv_tester.py')
+print('\nevv_tester_dataclasses.py')
 
-pulse_ir_1 = ws.experiment.abstractions_dataclasses.EmPulse('ideal', 1.0e-5, tc = 50.0, cf=0.00, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=1)
-pulse_ir_2 = ws.experiment.abstractions_dataclasses.EmPulse('impulsive', 1.0e-5, tc = 100.0, cf=None, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=2)
-pulse_uvvis_1 = ws.experiment.abstractions_dataclasses.EmPulse('ideal', 1.0e-5, tc = 120.0, cf=0.0, cf_uv=0.072, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=3)
+TO_PICKLES = ['VibExperiment', 'ExternalCalcSetup', 'WilsonSimulation']
+PKL_FILES = {}
 
-pulses = [pulse_ir_1, pulse_ir_2, pulse_uvvis_1]
+def run():
 
-field_a = ws.experiment.abstractions_dataclasses.ElectricField(pulses)
-order = len(pulses)
+    pulse_ir_1 = ws.experiment.abstractions_dataclasses.EmPulse('ideal', 1.0e-5, tc = 50.0, cf=0.00, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=1)
+    pulse_ir_2 = ws.experiment.abstractions_dataclasses.EmPulse('impulsive', 1.0e-5, tc = 100.0, cf=None, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=2)
+    pulse_uvvis_1 = ws.experiment.abstractions_dataclasses.EmPulse('ideal', 1.0e-5, tc = 120.0, cf=0.0, cf_uv=0.072, wv=[0.0, 0.0, 1.0], pol=[0.0, 0.0, 1.0], id=3)
 
-epochs = field_a.findEpochs()
+    pulses = [pulse_ir_1, pulse_ir_2, pulse_uvvis_1]
 
-detector_a = ws.experiment.abstractions_dataclasses.SpecDetector('freq', detector_location=[0.0, 0.0, 1.0],
-                                                     detection_polarization=[0.0, 0.0, 1.0],
-                                                     detection_range=[0.003 + 0.0001*i for i in range(101)],
-                                                     wv_filter=[{1: [-1], 2: [1], 3: [1]}]) #, {1: [-1], 2: [1], 3: [1]}
+    field_a = ws.experiment.abstractions_dataclasses.ElectricField(pulses)
+    order = len(pulses)
 
-# Push one carrier freq
-scan_obj_a = [['pulse', 1, 'cf', 1.0], ['detector', 0, 'detection_range', 1.0]]
-scan_range_a = [0.0001*i for i in range(101)]
-scan_a = ws.experiment.abstractions_dataclasses.SpecScan(scan_obj_a, scan_range_a)
+    field_a.findEpochs()
 
-experiment_a = ws.experiment.abstractions_dataclasses.VibExperiment(order, field_a, detector_a, [scan_a], magn_conditions=[[-1, 2]])
-print('\nexperiment_a.__dict__', experiment_a.__dict__, '\n')
+    detector_a = ws.experiment.abstractions_dataclasses.SpecDetector('freq', detector_location=[0.0, 0.0, 1.0],
+                                                        detection_polarization=[0.0, 0.0, 1.0],
+                                                        detection_range=[0.003 + 0.0001*i for i in range(101)],
+                                                        wv_filter=[{1: [-1], 2: [1], 3: [1]}]) #, {1: [-1], 2: [1], 3: [1]}
 
-calc_setup = ws.main.abstractions_dataclasses.ExternalCalcSetup(program='gaussian', lvl_theory='B3LYP', basis='cc_pVQZ')
+    # Push one carrier freq
+    scan_obj_a = [['pulse', 1, 'cf', 1.0], ['detector', 0, 'detection_range', 1.0]]
+    scan_range_a = [0.0001*i for i in range(101)]
+    scan_a = ws.experiment.abstractions_dataclasses.SpecScan(scan_obj_a, scan_range_a)
 
-sim = ws.main.abstractions_dataclasses.WilsonSimulation()
+    experiment_a = ws.experiment.abstractions_dataclasses.VibExperiment(order, field_a, detector_a, [scan_a], magn_conditions=[[-1, 2]])
+    if 'VibExperiment' in TO_PICKLES:
+        pickle_this_to(obj=experiment_a, filenamepkl='vibexp.pkl', save_to=SUITE_ROOT+'/tests/')
 
-sim.addExperiment(experiment_a)
-sim.getTerms(ws.derive.main.get_fully_enhanced_terms) # here terms are derived
-mol_system = ws.main.abstractions_dataclasses.MolecularSystem(name='ACAC', natoms=8)
-sim.addSystem(mol_system)
-# sim.addVibAnaSetup(ws.main.abstractions_dataclasses.vibAnaSetup(vib_regime='GVPT2', vibana_prop_need='anharm',
-#                                                     allow_skip_eigvec=True, external_fill_from=calc_setup))
-sim.addVibAnaSetup(ws.main.abstractions_dataclasses.VibAnaSetup(system=mol_system, regime='GVPT2', vibana_prop_need='none',
-                                                    allow_skip_eigvec=True, external_fill_from=calc_setup))
-sim.addPropEvalSetup(eval_uniform=calc_setup)
+        experiment_a = unpickle_smth_from(filenamepkl='vibexp.pkl', load_from=SUITE_ROOT+'/tests/')
+        PKL_FILES['VibExperiment'] = 'vibexp.pkl'
+    
+    calc_setup = ws.main.abstractions_dataclasses.ExternalCalcSetup(program='gaussian', lvl_theory='B3LYP', basis='cc_pVQZ')
+    if 'ExternalCalcSetup' in TO_PICKLES:
+        pickle_this_to(obj=calc_setup, filenamepkl='calcsetup.pkl', save_to=SUITE_ROOT+'/tests/')
 
-axis1 = ws.main.abstractions_dataclasses.SpectralAxis({1: 1})
-axis2 = ws.main.abstractions_dataclasses.SpectralAxis({1: 1, 2: -1})
-start = {1: 250, 2: 100}
-end = {1: 3850, 2: 7550}
-spacer = {1: 3.8, 2: 3.8}
-spec_grid = ws.main.abstractions_dataclasses.SpectralGrid({1: axis1, 2: axis2}, range_style='uniform',
-                                              start=start, end=end, spacer=spacer)
-evi = {'dynrange': 500, 'Gamma': 4.7, 'diag_margin': 5., 'maxmax': None}
-rndi = {'num_level_ticks': 15}
-eval_setup = ws.main.abstractions_dataclasses.SpecEvalSetup(grid=spec_grid, ev_info=evi, rnd_info=rndi)
-sim.addSpecEvalSetup(eval_setup)
+        calc_setup = unpickle_smth_from(filenamepkl='calcsetup.pkl', load_from=SUITE_ROOT+'/tests/')
+        PKL_FILES['ExternalCalcSetup'] = 'calcsetup.pkl'
 
-sim.findPropsAndMaxStateLvl() # setting up self.props/sim.props
-print('\nafter findPropsAndMaxStateLvl', sim.props, '\n')
+    sim = ws.main.abstractions_dataclasses.WilsonSimulation()
+    if 'WilsonSimulation_init' in TO_PICKLES:
+        pickle_this_to(obj=sim, filenamepkl='sim_init.pkl', save_to=SUITE_ROOT+'/tests/')
 
-sim.dressPropsWithSetup()
-sim.makeCalculationBatches()
-sim.getResultsFromCalculationBatches(source_type='vault',
-                                     source_loc=ws.intensities.utils.get_package_root()
-                                                + '/../tests/test_database/mini_files_database.csv' )
-print('\nafter getResultsFromCalculationBatches', sim.props, '\n')
+        sim = unpickle_smth_from(filenamepkl='sim_init.pkl', load_from=SUITE_ROOT+'/tests/')
+        PKL_FILES['WilsonSimulation_init'] = 'sim_init.pkl'
+    
+    sim.addExperiment(experiment_a)
+    sim.getTerms(ws.derive.main.get_fully_enhanced_terms) # here terms are derived
+    mol_system = ws.main.abstractions_dataclasses.MolecularSystem(name='FORM', natoms=4)
+    sim.addSystem(mol_system)
+    sim.addVibAnaSetup(ws.main.abstractions_dataclasses.VibAnaSetup(system=mol_system, regime='GVPT2', vibana_prop_need='none',
+                                                        allow_skip_eigvec=True, external_fill_from=calc_setup))
+    sim.addPropEvalSetup(eval_uniform=calc_setup)
+
+    axis1 = ws.main.abstractions_dataclasses.SpectralAxis({1: 1})
+    axis2 = ws.main.abstractions_dataclasses.SpectralAxis({1: 1, 2: -1})
+    start = {1: 250, 2: 100}
+    end = {1: 3850, 2: 7550}
+    spacer = {1: 3.8, 2: 3.8}
+    spec_grid = ws.main.abstractions_dataclasses.SpectralGrid({1: axis1, 2: axis2}, range_style='uniform',
+                                                start=start, end=end, spacer=spacer)
+    evi = {'dynrange': 500, 'Gamma': 4.7, 'diag_margin': 5., 'maxmax': None}
+    rndi = {'num_level_ticks': 15}
+    eval_setup = ws.main.abstractions_dataclasses.SpecEvalSetup(grid=spec_grid, ev_info=evi, rnd_info=rndi)
+    sim.addSpecEvalSetup(eval_setup)
+
+    sim.findPropsAndMaxStateLvl() # setting up self.props/sim.props
+    print('\nafter findPropsAndMaxStateLvl', sim.props, '\n')
+
+    sim.dressPropsWithSetup()
+    sim.makeCalculationBatches()
+    sim.getResultsFromCalculationBatches(source_type='vault',
+                                        source_loc=ws.intensities.utils.get_package_root()
+                                                    + '/../tests/test_database/mini_files_database.csv' )
+    print('\nafter getResultsFromCalculationBatches', sim.props, '\n')
 
 
-print('\n===========================================================================')
-import numpy as np
-np.set_printoptions(precision=4)
-from pathlib import Path
-my_file = Path("./spec.npy")
+    print('\n===========================================================================')
+    import numpy as np
+    np.set_printoptions(precision=4)
 
-if my_file.is_file():
-    print('  >>> sim.spec data loaded from spec.npy file...\n')
-    ampl = np.load('spec.npy')
-    intensities_spec = np.abs(ampl)**2
-else:
+    if 'WilsonSimulation_mid' in TO_PICKLES:
+        pickle_this_to(obj=sim, filenamepkl='sim_mid.pkl', save_to=SUITE_ROOT+'/tests/')
+
+        sim = unpickle_smth_from(filenamepkl='sim_mid.pkl', load_from=SUITE_ROOT+'/tests/')
+        PKL_FILES['WilsonSimulation_mid'] = 'sim_mid.pkl'
+
     print('  >>> Going to evaluate now...\n')
-    # sim.evaluate(ws.intensities.spectrum.wilsonmain_integration.spectrum2D)
     sim.evaluateAsResponseFunction(evaluator=ws.intensities.spectrum.evaluators.terms_evaluator)
     intensities_spec = np.abs(sim.spec)**2
     print(np.max(np.abs(sim.spec)**2))
-    np.save('spec.npy', sim.spec)
 
-print('\n===========================================================================')
-print('\n  >>> And now rendering...\n')
+    print('\n===========================================================================')
+    print('\n  >>> And now rendering...\n')
 
-# sim.render(ws.intensities.wilsonmain_render_integration.render_spectrum)
-from wilson_analysis.render import render_spectrum
+    from wilson_analysis.render import render_spectrum
 
-hist, bin_edges = np.histogram(intensities_spec, bins=10)
-print("Histogram counts:", hist)
-print("Bin edges:", bin_edges)
-print('\n')
+    hist, bin_edges = np.histogram(intensities_spec, bins=10)
+    print("Histogram counts:", hist)
+    print("Bin edges:", bin_edges)
+    print('\n')
 
-# sim.render(ws.intensities.wilsonmain_render_integration.render_spectrum)
-print(f'np.max(intensities): {np.max(intensities_spec):.4e}')
+    print(f'np.max(intensities): {np.max(intensities_spec):.4e}')
 
-dict_meshes = spec_grid.make_mesh_numpy()
-render_spectrum(intensities_spec, dict_meshes[1], dict_meshes[2],
-                filename='yo_terms_derive_ACAC.svg', dynamic_range=100,
-                nicetitle='TermsEvaluator')
+    dict_meshes = spec_grid.make_mesh_numpy()
+    render_spectrum(intensities_spec, dict_meshes[1], dict_meshes[2],
+                    filename='yo_terms_derive_ACAC.svg', dynamic_range=100,
+                    nicetitle='TermsEvaluator')
 
-from wilson_utils.serialization import check_if_jsonsafe
+    from wilson_utils.serialization import check_if_jsonsafe
+    check_if_jsonsafe(sim.to_dict())
 
-# print(sim.props[0].vals)
-# exit()
-for i in sim.props:
-    i.make_serial_vals()
-print(sim.props[0].serial_vals)
+    ws_root = ws.intensities.utils.get_package_root() + '/../../'
 
-check_if_jsonsafe(sim.to_dict())
-simdict = sim.to_dict()
-for k in simdict:
-    print(k)
-    if isinstance(simdict[k], dict):
-        print(simdict[k].keys())
-        if k=='calc_batches':
-            print(simdict['calc_batches'])
-    elif isinstance(simdict[k], list) and simdict[k]:
-        if isinstance(simdict[k][0], dict):
-            print(simdict[k][0].keys())
-    else:
-        print(simdict[k])
-    print('-------')
+    # writing JSON file
+    sim.writeToJsonFile(ws_root+'/tests/WilsonSimulation.json')
 
-ws_root = ws.intensities.utils.get_package_root() + '/../../'
+    # pickling
+    import pickle
+    with open(ws_root+"/tests/wilsonsim0.pkl", "wb") as f:
+        pickle.dump(sim, f)
 
-# writing JSON file
-sim.writeToJsonFile(ws_root+'/tests/WilsonSimulation.json')
+    with open(ws_root+"/tests/wilsonsim0.pkl", "rb") as f:
+        loaded_wilsonsim0 = pickle.load(f)
+    
+    # apparently numpy can't compare complex numbers
+    assert np.allclose(np.abs(sim.spec), np.abs(loaded_wilsonsim0.spec))
 
-# pickling
-import pickle
-with open(ws_root+"/tests/wilsonsim0.pkl", "wb") as f:
-    pickle.dump(sim, f)
-
-with open(ws_root+"/tests/wilsonsim0.pkl", "rb") as f:
-    loaded_wilsonsim0 = pickle.load(f)
-
-assert sim.spec == loaded_wilsonsim0.spec
+    return sim, ws_root+"/tests/wilsonsim0.pkl"
