aa8d8b152b406b8668c6b55e8ec0c864d3e6105a
Vanda Le <vandwlev@gmail.com>
2025-07-24 02:35:01 +0200

    SUITE_ROOT name

--- tests/test_serialization_utils.py
+++ tests/test_serialization_utils.py
@@ -67,15 +67,15 @@ def test_find_non_json_safe():
 
 def test_get_utils_root():
     import os
-    from wilson_utils.paths import UTILS_ROOT, SUITE_root
+    from wilson_utils.paths import UTILS_ROOT, SUITE_ROOT
 
     assert os.path.isabs(UTILS_ROOT)
     assert os.path.exists(UTILS_ROOT)
     assert os.path.basename(UTILS_ROOT) == "wilson_utils"
     
-    assert os.path.basename(SUITE_root) == 'wilson-suite'
-    assert os.path.isabs(SUITE_root)
-    assert os.path.exists(SUITE_root)
+    assert os.path.basename(SUITE_ROOT) == 'wilson-suite'
+    assert os.path.isabs(SUITE_ROOT)
+    assert os.path.exists(SUITE_ROOT)
 
 @dataclass
 class Mock1:
--- wilson_utils/paths.py
+++ wilson_utils/paths.py
@@ -1,3 +1,3 @@
 import os
 UTILS_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SUITE_root = os.path.abspath(os.path.join(UTILS_ROOT, '..'))
+SUITE_ROOT = os.path.abspath(os.path.join(UTILS_ROOT, '..'))
