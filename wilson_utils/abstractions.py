ed8bbc8459cbe72b52aed68eb14549a372502702
ringholm <mringholm@gmail.com>
2025-07-25 14:05:53 +0200

    upd VibState class to dataclass

--- wilson_utils/abstractions.py
+++ wilson_utils/abstractions.py
@@ -1,24 +1,36 @@
 from typing import Any
 
-# State, energy, displacement
+@dataclass
 class VibState:
 	"""
 	Class to represent a vibrational state.
 	This is for a "concrete" vibrational state and not the same as its symbolic namesake in wilson-derive.
+
+	----
+	s: dictionary {(harm. quanta): coeff, (harm. quanta): coeff, ...}: Specify the state in terms of harm. osc. WFs
+	e: float: State energy level
+	d: type not specified: Should be some form of vector to represent displacement in terms of atomic coordinates
+	
+	UPD:
+	dictionary self.s is not JSON-serializable (tuples can't be keys), but self.serial_s is.
+	self.serial_s is set up in post_init; deserialize_state_dict will return original self.s based on self.serial_s.
+
+	Notes:
+	s: InitVar[dict] = field(repr=False) - means that this atribute will not be in repr() of the class instance
+	InitVar - is an init-only variable
+	This seems to be okay for now, but should mind this feature
 	"""
+	s: InitVar[dict] = field(repr=False)
+	e: float = 0.0
+	d: Any = None
+	serial_s: dict = field(init=False)
 
-	def __init__(self, s: dict, e: float, d: Any=None):
-		"""
-		s: dictionary {(harm. quanta): coeff, (harm. quanta): coeff, ...}: Specify the state in terms of harm. osc. WFs
-		e: float: State energy level
-		d: type not specified: Should be some form of vector to represent displacement in terms of atomic coordinates.
-		"""
+	def __post_init__(self, s):
+		self.serial_s = {",".join(k): v for k, v in s.items()}
 
-		self.s = s
-		self.e = e
-		self.d = d
+	# def __post_init__(self):
+		# self.serial_s = {",".join(k): v for k, v in self.s.items()}
 
-	def __repr__(self):
-		return f"vibState {self.s}, energy is {self.e} cm-1"
-
+	def deserialize_state_dict(self) -> dict:
+		return {tuple(k.split(",")): v for k, v in self.serial_s.items()}
 
