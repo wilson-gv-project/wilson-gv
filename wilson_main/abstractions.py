b710b9f549e04251409dfbed180c154a60db03d4
Vanda Le <vandwlev@gmail.com>
2025-07-25 09:04:38 +0200

    upd naharm analyzer context

--- wilson_main/abstractions.py
+++ wilson_main/abstractions.py
@@ -504,7 +504,7 @@ class VibAnaSetup:
 
 		context = {'system': self.system, 'props': props, 
 			 		'regime': self.regime, 'regime_subinfo': self.regime_subinfo, 
-					'nc_sqrt_eigval': self.nc_sqrt_eigval, 'nc_eigvec': self.nc_eigvec,
+					'nc_sqrt_eigval': self.nc_sqrt_eigval, 
 					'exclude_modes': self.exclude_modes}
 		self.states = anharmonic_analyzer(**context)
 
