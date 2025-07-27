84783727d4f30eb7c2a45f824143adf3a39a922f
Vanda Le <vandwlev@gmail.com>
2025-07-27 12:10:33 +0200

    prepare return: list of VibStates and diagnostics dict; commented out detailed analyzer func

--- tests/unit/test_anharm_analyzer.py
+++ tests/unit/test_anharm_analyzer.py
@@ -45,50 +45,29 @@ def test_anharm_analyzer():
     mol_system = ws_main.abstractions.MolecularSystem(name='FORM', natoms=4)
     calc_setup = ws_main.abstractions.ExternalCalcSetup(program='gaussian', lvl_theory='B3LYP', basis='cc_pVQZ')
     
-    # for gvpt2 need both vib_regime and vibana_prop_need='all', otherwise no coriolis and rot
-    # need vibana_prop_need='anharm' for nc_sqrt_eigval to be retrieved
-    vibana = ws_main.abstractions.VibAnaSetup(system=mol_system, regime='GVPT2', vibana_prop_need='anharm',
-                                                    allow_skip_eigvec=True, external_fill_from=calc_setup)
+    vibana = ws_main.abstractions.VibAnaSetup(system=mol_system, regime='GVPT2',
+                                              vibana_prop_need='anharm',
+                                              allow_skip_eigvec=True, external_fill_from=calc_setup)
     printtest(f'vibana.vibana_prop_need: {vibana.vibana_prop_need}')
 
     experiment_a = evv_experiment()
 
     sim = ws_main.abstractions.WilsonSimulation()
     sim.addExperiment(experiment_a)
-    sim.getTerms(ws_derive.main.get_fully_enhanced_terms) # here terms are derived
+    sim.getTerms(ws_derive.main.get_fully_enhanced_terms)
     
     sim.addSystem(mol_system)
     sim.addPropEvalSetup(eval_uniform=calc_setup)
     sim.addVibAnaSetup(vib_ana_setup=vibana)
     sim.addPropEvalSetup(eval_uniform=calc_setup)
-    # should take care of when props are there, because needed for vib analyzer
+    # should be careful with props, because props are needed for vib analyzer
     sim.findPropsAndMaxStateLvl() # setting up self.props/sim.props
     printtest(f'[i.triv_name for i in sim.props] {[i.trivial_name for i in sim.props]}')
     sim.dressPropsWithSetup()
     sim.makeCalculationBatches()
     sim.getResultsFromCalculationBatches(source_type='vault',
                                         source_loc=SUITE_ROOT+'/wilson_intensities/tests/test_database/mini_files_database.csv' )
-    printtest(f'sim.props {sim.props}')
-    printtest(f'MolecularProperty {sim.props[0].__dict__.keys()}')
-    for i in sim.props:
-        if i.trivial_name == 'cff':
-            printtest(i.vals.shape)
-    
-    # ['prop_spec', 'triv_name', 'vals', 'in_basis', 'in_units', 
-    # 'system', 'calc_setup', 'target_basis', 'target_units']
-    printtest(f'sim.vib_ana_setup.nc_sqrt_eigval: {sim.vib_ana_setup.nc_sqrt_eigval}')
-    printtest(f'len(sim.vib_ana_setup.nc_sqrt_eigval): {len(sim.vib_ana_setup.nc_sqrt_eigval)}')
 
+    # FIXME: should be done internally with WilsonSimulation somehow? or when?
     sim.vib_ana_setup.doAnharmonicAnalysis(sim.props, anharmonic_analyzer=anharm_analyzer_data)
 
-def test_anh_ana():
-    separator_print()
-    import logging
-    from wilson_utils.logger import setup_logger
-    setup_logger("wilson", level=logging.DEBUG)
-    logging.getLogger('wilson.wilson.spectrum.vpt2').setLevel(logging.INFO)
-
-    from CQCParse.logger import setup_logger as set_loggerCQCP
-    set_loggerCQCP('CQCParse', level=logging.ERROR)
-
-    from wilson.spectrum.anharmonic_analyzer import anharm_analyzer
--- wilson/spectrum/anharmonic_analyzer.py
+++ wilson/spectrum/anharmonic_analyzer.py
@@ -1,43 +1,39 @@
 """
-(self.system, props, self.regime, self.regime_subinfo, self.nc_sqrt_eigval, self.nc_eigvec)
+Anharmonic analyzer for wilson_main.VibAnaSetup.doAnharmonicAnalysis()
+
 """
-import copy
-import numpy as np
-from wilson_main.abstractions import VibState, MolecularSystem, MolecularProperty
+from wilson_utils.abstractions import VibState
+from wilson_main.abstractions import MolecularSystem, MolecularProperty
 from wilson.spectrum.vpt2 import anharm_corr_energies
-from wilson.spectrum.vpt2 import identify_fermi, adjust_for_fermi_resonance, get_X
 
 import logging
 logger = logging.getLogger("wilson."+__name__)
 
 def anharm_analyzer_data(system:MolecularSystem = None, props: list[MolecularProperty] = None, 
                     nc_sqrt_eigval: dict = None, 
-                    regime: dict = None, regime_subinfo: dict = None, 
-                    exclude_modes: list = None) -> list[VibState]:
+                    regime: str = None, regime_subinfo: dict = None, 
+                    exclude_modes: list = None) -> tuple[list[VibState], dict]:
     """
     Basically a wrapper for analyser; passes data to anharm_corr_energies where analysis happens...
         then puts into list[VibState] form
 
-    returns self.states for VibAnaSetup, 
-        and states: list[VibState]
+    returns self.states for VibAnaSetup, and states are list[VibState]
     
-            Returns VPT2 corrected energy levels of all states as a dictionary : {str(int): float}
-
     """
     logger.info('Starting anharm_analyzer()')
     
     # modes exclusion list update
     if exclude_modes is None:
         exclude_modes = []
-
+    
     prop_dict = {i.trivial_name: i.vals for i in props}
     logger.debug(f'prop_dict {prop_dict.keys()}')
 
     # corrected_levels : funds, over2q, combo2q, over3q, combo3q
-    # corrected_levels = anharm_corr_energies(upd_harmonic_energies,
-    corrected_levels, fermi_resonance = anharm_corr_energies(nc_sqrt_eigval,
+    corrected_levels, fermi_resonances = anharm_corr_energies(nc_sqrt_eigval,
                                                              prop_dict['cff'], prop_dict['qff'], prop_dict['B'], prop_dict['coriolis'],
                                                              regime, exclude_modes)
+    # assembling data for return
     all_states_corr = {}
 
     for i in range(len(nc_sqrt_eigval)):
@@ -56,128 +52,132 @@ def anharm_analyzer_data(system:MolecularSystem = None, props: list[MolecularPro
                     key = tuple([str(el) for el in sorted([i, j, k])])
                     if key not in all_states_corr:
                         if corrected_levels[4][i, j, k] != 0.:
-                            all_states_corr[key] = corrected_levels[4][
-                                i, j, k]
+                            all_states_corr[key] = corrected_levels[4][i, j, k]
 
-    all_states = copy.deepcopy(all_states_corr)
-    one = {i: all_states[i] for i in all_states if len(i) == 1}
-    two = {i: all_states[i] for i in all_states if len(i) == 2}
+    logger.debug(f'all_states_corr: {all_states_corr}')
 
-    logger.info('GVPT2 anharm corrected:')
-    logger.info(repr(dict(sorted(one.items()))))
-    logger.info(repr(dict(sorted(two.items()))))
+    vibstates = []
+    for st in all_states_corr:
+        # if len(st) <= max_state_lvl: # ?
+        # TODO: Exclusion based on mode index or freq cutoff
+        # FIXME: Change to integer indexing - in s dict?
+        vibstates.append(VibState(s={st: 1.0}, e=all_states_corr[st]))
 
-    return all_states, fermi_resonance
+    logger.debug('GVPT2 anharm corrected:')
+    logger.debug(f'vibstates: {vibstates}')
 
-
-def anharm_analyzer(system:MolecularSystem = None, props: list[MolecularProperty] = None, 
-                    nc_sqrt_eigval: dict = None, 
-                    regime: dict = None, regime_subinfo: dict = None, 
-                    exclude_modes: list = None) -> list[VibState]:
-# def anharm_corr_energies(harmonic_energies, cubic_forcefield, quartic_forcefield,
-#                          rotational_constant, coriolis_constant, anharmonic_type,
-#                          list2exclude):
-    """
-    Takes in cm-1 unit for all the arguments:
-    UPD! harmonic_energies is a dictionary - parserObj.fundamentals_harmonic_int
-
-        harmonic_energies, cubic_forcefield, quartic_forcefield, rotational_constant, coriolis_constant(unit?)
-        (nmodes,);   (nmodes, nmodes, nmodes);   (nmodes, nmodes, nmodes, nmodes);   [x,y,z];   (nmodes, nmodes)
-
-    anharmonic_type options:
-            'VPT2'                        - don't do_res, don't do_var
-            'DVPT2'                       - do_res, don't do_var
-            'GVPT2'                       - do_res, do_var
-    returns:
-        fundamental, overtones, combotones, over3q, combo3q
-    """
-
-    if anharmonic_type == 'GVPT2':
-        do_variational_correction = True
-        do_resonance_checks = True
-    elif anharmonic_type == 'VPT2':
-        do_resonance_checks = False
-        do_variational_correction = False
-    elif anharmonic_type == 'DVPT2':
-        do_resonance_checks = True
-        do_variational_correction = False
-    else:
-        raise AssertionError("Unrecornized anharmonic type or it isn't specified")
-
-    harmonic_energies = list(nc_sqrt_eigval.values())
-
-    original_len_ene = len(harmonic_energies)
-    harmonic_energies = {k: v for k, v in harmonic_energies.items() if k not in exclude_modes}
-
-    fundamental = np.zeros((original_len_ene))
-    overtones = np.zeros((original_len_ene))
-    combotones = np.zeros((original_len_ene, original_len_ene))
-    over3q = np.zeros((original_len_ene))
-    combo3q = np.zeros((original_len_ene, original_len_ene, original_len_ene))
-
-    fermi_resonance = identify_fermi(harmonic_energies, cubic_forcefield, do_resonance_checks)
-    # selecting resonances fermi_resonance = [fermi_resonance[0]]
-    X, X_cubic, X_quartic, X_coriolis = get_X(harmonic_energies, cubic_forcefield, quartic_forcefield,
-                                              rotational_constant, coriolis_constant, do_resonance_checks,
-                                              fermi_resonance, original_len_ene)
+    diagnostics = {'fermi_resonances': fermi_resonances}
+    
+    return vibstates, diagnostics
 
 
-    if fermi_resonance: # if not an empty list
-        logger.debug(f'Fermi resonances identified - {len(fermi_resonance)}: {fermi_resonance}')
+# def anharm_analyzer(system:MolecularSystem = None, props: list[MolecularProperty] = None,
+#                     nc_sqrt_eigval: dict = None,
+#                     regime: str = None, regime_subinfo: dict = None,
+#                     exclude_modes: list = None) -> list[VibState]:
+#     """
+#     Takes in cm-1 unit for all the arguments:
+#     UPD! harmonic_energies is a dictionary - parserObj.fundamentals_harmonic_int
 
-    funds_corrections = np.zeros((original_len_ene))
-    # for i in range(len(harmonic_energies)):
-    for i in harmonic_energies:
-        fundamental[i] += harmonic_energies[i] + 2 * X[i][i]
+#         harmonic_energies, cubic_forcefield, quartic_forcefield, rotational_constant, coriolis_constant(unit?)
+#         (nmodes,);   (nmodes, nmodes, nmodes);   (nmodes, nmodes, nmodes, nmodes);   [x,y,z];   (nmodes, nmodes)
 
-        fscr = 0
-        # for j in range(len(harmonic_energies)):
-        for j in harmonic_energies:
-            if j != i:
-                fscr += 0.5 * X[i][j]
+#     anharmonic_type options:
+#             'VPT2'                        - don't do_res, don't do_var
+#             'DVPT2'                       - do_res, don't do_var
+#             'GVPT2'                       - do_res, do_var
+#     returns:
+#         fundamental, overtones, combotones, over3q, combo3q
+#     """
 
-        fundamental[i] += fscr
-        funds_corrections[i] += 2 * X[i][i] + fscr
+#     if regime == 'GVPT2':
+#         do_variational_correction = True
+#         do_resonance_checks = True
+#     elif regime == 'VPT2':
+#         do_resonance_checks = False
+#         do_variational_correction = False
+#     elif regime == 'DVPT2':
+#         do_resonance_checks = True
+#         do_variational_correction = False
+#     else:
+#         raise AssertionError("Unrecornized anharmonic type or it isn't specified")
 
-    overtones_corrections = np.zeros((original_len_ene))
-    combotones_corrections = np.zeros((original_len_ene, original_len_ene))
-    over3q_corrections = np.zeros((original_len_ene))
-    combo3q_corrections = np.zeros((original_len_ene, original_len_ene, original_len_ene))
+#     if exclude_modes is None:
+#         exclude_modes = []
 
-    # for i in range(len(harmonic_energies)):
-    for i in harmonic_energies:
-        overtones[i] += 2 * fundamental[i] + 2 * X[i][i]
-        overtones_corrections[i] += 2 * X[i][i]
+#     original_len_ene = len(nc_sqrt_eigval)
+#     harmonic_energies = {k: v for k, v in nc_sqrt_eigval.items() if k not in exclude_modes}
 
-        over3q[i] += 3 * fundamental[i] + 6 * X[i][i]
-        over3q_corrections[i] += 6 * X[i][i]
+#     fundamental = np.zeros((original_len_ene))
+#     overtones = np.zeros((original_len_ene))
+#     combotones = np.zeros((original_len_ene, original_len_ene))
+#     over3q = np.zeros((original_len_ene))
+#     combo3q = np.zeros((original_len_ene, original_len_ene, original_len_ene))
 
-        # for j in range(len(harmonic_energies)):
-        for j in harmonic_energies:
-            if i == j:
-                # for k in range(len(harmonic_energies)):
-                for k in harmonic_energies:
-                    if k != i:
-                        combo3q[i][i][k] += 2 * fundamental[i] + 2 * X[i][i] + fundamental[k] + 2 * X[i][k]
-                        combo3q_corrections[i][i][k] += 2 * X[i][i] + 2 * X[i][k]
+#     prop_dict = {i.trivial_name: i.vals for i in props}
+#     logger.debug(f'prop_dict {prop_dict.keys()}')
 
-            else:
-                combotones[i][j] += fundamental[i] + fundamental[j] + X[i][j]
-                combotones_corrections[i][j] += X[i][j]
+#     cubic_forcefield = prop_dict['cff']
+#     quartic_forcefield = prop_dict['qff']
+#     rotational_constant = prop_dict['B']
+#     coriolis_constant = prop_dict['coriolis']
 
-                # for k in range(len(harmonic_energies)):
-                for k in harmonic_energies:
-                    if k == i or k == j:
-                        continue
-                    combo3q[i][j][k] += fundamental[i] + fundamental[j] + fundamental[k] + X[i][j] + X[i][k] + X[j][k]
-                    combo3q_corrections[i][j][k] += X[i][j] + X[i][k] + X[j][k]
+#     fermi_resonance = identify_fermi(harmonic_energies, cubic_forcefield, do_resonance_checks)
+#     # selecting resonances fermi_resonance = [fermi_resonance[0]]
+#     X, X_cubic, X_quartic, X_coriolis = get_X(harmonic_energies, cubic_forcefield, quartic_forcefield,
+#                                               rotational_constant, coriolis_constant, do_resonance_checks,
+#                                               fermi_resonance, original_len_ene)
 
-    if do_variational_correction:
-        selectedFR = range((len(fermi_resonance)))
-        adjusted_fundamental, adjusted_overtones, adjusted_combotones = \
-            adjust_for_fermi_resonance(fundamental, overtones, combotones, over3q, combo3q, cubic_forcefield,
-                                       [fermi_resonance[i] for i in selectedFR])
-        return (adjusted_fundamental, adjusted_overtones, adjusted_combotones, over3q, combo3q), fermi_resonance
+#     if fermi_resonance: # if not an empty list
+#         logger.debug(f'Fermi resonances identified - {len(fermi_resonance)}: {fermi_resonance}')
 
-    else:
-        return (fundamental, overtones, combotones, over3q, combo3q), fermi_resonance
+#     funds_corrections = np.zeros((original_len_ene))
+#     for i in harmonic_energies:
+#         fundamental[i] += harmonic_energies[i] + 2 * X[i][i]
+
+#         fscr = 0
+#         for j in harmonic_energies:
+#             if j != i:
+#                 fscr += 0.5 * X[i][j]
+
+#         fundamental[i] += fscr
+#         funds_corrections[i] += 2 * X[i][i] + fscr
+
+#     overtones_corrections = np.zeros((original_len_ene))
+#     combotones_corrections = np.zeros((original_len_ene, original_len_ene))
+#     over3q_corrections = np.zeros((original_len_ene))
+#     combo3q_corrections = np.zeros((original_len_ene, original_len_ene, original_len_ene))
+
+#     for i in harmonic_energies:
+#         overtones[i] += 2 * fundamental[i] + 2 * X[i][i]
+#         overtones_corrections[i] += 2 * X[i][i]
+
+#         over3q[i] += 3 * fundamental[i] + 6 * X[i][i]
+#         over3q_corrections[i] += 6 * X[i][i]
+
+#         for j in harmonic_energies:
+#             if i == j:
+#                 for k in harmonic_energies:
+#                     if k != i:
+#                         combo3q[i][i][k] += 2 * fundamental[i] + 2 * X[i][i] + fundamental[k] + 2 * X[i][k]
+#                         combo3q_corrections[i][i][k] += 2 * X[i][i] + 2 * X[i][k]
+
+#             else:
+#                 combotones[i][j] += fundamental[i] + fundamental[j] + X[i][j]
+#                 combotones_corrections[i][j] += X[i][j]
+
+#                 for k in harmonic_energies:
+#                     if k == i or k == j:
+#                         continue
+#                     combo3q[i][j][k] += fundamental[i] + fundamental[j] + fundamental[k] + X[i][j] + X[i][k] + X[j][k]
+#                     combo3q_corrections[i][j][k] += X[i][j] + X[i][k] + X[j][k]
+
+#     if do_variational_correction:
+#         selectedFR = range((len(fermi_resonance)))
+#         adjusted_fundamental, adjusted_overtones, adjusted_combotones = \
+#             adjust_for_fermi_resonance(fundamental, overtones, combotones, over3q, combo3q, cubic_forcefield,
+#                                        [fermi_resonance[i] for i in selectedFR])
+#         return (adjusted_fundamental, adjusted_overtones, adjusted_combotones, over3q, combo3q), fermi_resonance
+
+#     else:
+#         return (fundamental, overtones, combotones, over3q, combo3q), fermi_resonance
