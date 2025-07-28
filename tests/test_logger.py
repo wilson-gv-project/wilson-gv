356c4f4269259c4ac1970389623b16795c0b4800
Vanda Le <vandwlev@gmail.com>
2025-07-28 11:06:06 +0200

    added logger message argument test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -16,8 +16,6 @@ def test_logger_with_caplog_serialization(caplog):
     with caplog.at_level(logging.INFO, logger="wilson.wilson_utils.serialization"):
         check_if_jsonsafe(Mock(1, {(0,1): 'str'}))
     # Check the captured log messages
-    printtest(caplog.records)
-    printtest(len(caplog.records))
     assert len(caplog.records) == 2
     assert caplog.records[0].levelname == "WARNING"
     assert caplog.records[0].message == "🔍 Offending object: {'a': 1, 'b': {(0, 1): 'str'}}"
@@ -26,3 +24,25 @@ def test_logger_with_caplog_serialization(caplog):
     log_output = caplog.text.replace("  ", " ")  # Replace multiple spaces with a single space
     assert "WARNING wilson.wilson_utils.serialization:serialization.py:60 🔍 Offending object: {'a': 1, 'b': {(0, 1): 'str'}}" in log_output
 
+def logfunc_mock():
+    import logging
+    logger = logging.getLogger('testlogger')
+    # Intentionally incorrect usage of logger.info: argument should be one string or a formatted string
+    logger.info('', '', 'fg')
+
+
+def test_log_messages():
+    """
+    Trying to show that argument to logger message functions should be one string or a formatted string.
+    """
+    import pytest
+    from wilson_utils.logger import setup_logger
+    setup_logger('testlogger', level=logging.INFO)
+    # test logfunc_mock and ensure it raises a specific error if the log message is incorrect
+    try:
+        logfunc_mock()
+        # fail test if no error raised
+        pytest.fail("logfunc_mock() did not raise an error when it should have")
+    except TypeError as e:
+        assert "not all arguments converted during string formatting" in str(e), \
+            f"Unexpected error message: {e}"
