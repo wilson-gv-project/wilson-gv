835140cb5fcba57119e133aa220a8d35bc001bbc
Vanda Le <vandwlev@gmail.com>
2025-08-07 19:16:45 +0200

    mv comments from wilson-main

--- wilson_analysis/render/render.py
+++ wilson_analysis/render/render.py
@@ -18,6 +18,21 @@ def render_spectrum(context: 'SimContext') -> None:
                 'name': self.name, 
                 'spec_eval_setup': self.spec_eval_setup,
                 'do_diagn': True}
+--------------
+
+    what are the options for rendering? 
+    
+    by plot type/projection type
+    1. 1D plot : slice of a 2D spectrum; 1D IR/Raman spectrum (?)
+    2. 2D plot : 2D IR/Raman spectrum; slice of a 3D spectrum : a) contour plot; b) scatter plot
+    3. 3D plot : 2D IR/Raman spectrum : surface plot == contout plot (same info; Z axis is intensity or color is intensity)
+
+    by spectrum dimensionality
+    - 1D spectrum: 1D IR/Raman spectrum - simply 1d plot
+    - 2D spectrum: 2D IR/Raman spectrum - 2D contour/scatter plot or 3D surface plot
+    - 3D spectrum: 3D IR/Raman spectrum - 1) 2D slice of a 3D spectrum - 2D contour/scatter plot or 3D surface plot; 2) 3D surface plot with color as intensity
+    - nD spectrum: nD IR/Raman spectrum - lower D slices as above
+    
     """
     spec_data = context.spec
     spec_eval_setup = context.spec_eval_setup
