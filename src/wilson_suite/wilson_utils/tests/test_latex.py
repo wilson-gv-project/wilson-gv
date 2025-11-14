7a9ce5e154cc67c2dce693b09faffc21cf893093
Vanda Le <vandwlev@gmail.com>
2025-11-14 10:16:25 +0100

    rm latex show figs

--- src/wilson_suite/wilson_utils/tests/test_latex.py
+++ src/wilson_suite/wilson_utils/tests/test_latex.py
@@ -8,12 +8,12 @@ def test_plt_latex():
     expr5 = r'\frac{1}{4}\frac{1}{(\omega_{,a} -B)(\omega_{,a} -A+B)}'
     
     expr6 = r'\frac{-1}{4}\frac{1}{\omega_{a,}\omega_{b,}}\frac{\partial\mu_{\beta}}{\partial Q_{a}}\frac{\partial\mu_{\gamma}}{\partial Q_{b}}\frac{\partial^{2}\alpha_{\alpha\delta}}{\partial Q_{a}\partial Q_{b}}\frac{1}{(\omega_{,a} +A-B)(\omega_{b,a} -B)}'
-    get_plt_latex(expr1)
-    get_plt_latex(expr2)
-    get_plt_latex(expr3)
-    get_plt_latex(expr4)
-    get_plt_latex(expr5)
-    get_plt_latex(expr6)
+    # get_plt_latex(expr1)
+    # get_plt_latex(expr2)
+    # get_plt_latex(expr3)
+    # get_plt_latex(expr4)
+    # get_plt_latex(expr5)
+    # get_plt_latex(expr6)
 
 def test_saved():
     from wilson_suite.fixtures import get_terms_from_json
