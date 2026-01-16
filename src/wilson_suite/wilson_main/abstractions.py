0236b08c8704df14e6a3c520a13f2a292d6fa967
Vanda Le <vandwlev@gmail.com>
2026-01-16 11:41:16 +0100

    one more print upd

--- src/wilson_suite/wilson_main/abstractions.py
+++ src/wilson_suite/wilson_main/abstractions.py
@@ -338,7 +338,7 @@ class VibAnaSetup:
 		if self.nc_sqrt_eigval is not None and self.has_all_states:
 			return True
 		print('\nself.nc_sqrt_eigval is not None', self.nc_sqrt_eigval is not None)
-		print('self.has_all_states', self.has_all_states)
+		print('self.has_all_states', self.has_all_states, '\n')
 		return False
 	
 	def setStates(self, states: list[VibState]):
