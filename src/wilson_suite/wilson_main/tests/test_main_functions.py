0b6577bf3e033f3da0fff7000c7a86d1cd40d75a
Vanda Le <vandwlev@gmail.com>
2026-01-19 15:16:19 +0100

    upd tests and move test file

---
+++ src/wilson_suite/wilson_main/tests/test_main_functions.py
@@ -0,0 +1,48 @@
+import wilson_suite as ws
+from ...fixtures import evv_experiment
+
+def test_find_props():
+    experiment_a = evv_experiment()
+    terms = ws.derive.derive.get_fully_enhanced_terms(experiment=experiment_a)
+    props = ws.main.main_functions.find_props(terms=terms)
+
+    for p in props:
+        print(p)
+
+def test_find_residual_vib_info():
+
+    vib_ana = ws.main.abstractions.VibAnaSetup(system='', regime='GVPT2', vibana_own_analysis='anharm')
+    props = ws.main.main_functions.find_residual_vib_info(vib_ana=vib_ana)
+
+    for p in props:
+        print(p)
+
+def test_find_props_and_max_state_lvl():
+    print()
+    vib_ana = ws.main.abstractions.VibAnaSetup(system='', regime='GVPT2', vibana_own_analysis='anharm')
+    
+    experiment_a = evv_experiment()
+    terms = ws.derive.derive.get_fully_enhanced_terms(experiment=experiment_a)
+    props, _, _ = ws.main.main_functions.find_props_and_max_state_lvl(terms=terms, vib_ana=vib_ana)
+    print([k.h(1) for k in props], '\n')
+    print(set([k.h(1) for k in props]), '\n')
+    
+    for p in props:
+        print(p)
+    if not 'f' in 'fjk':
+        print()
+
+def test_get_data_for_vibanalysers():
+    print()
+    vib_ana = ws.main.abstractions.VibAnaSetup(system='', regime='GVPT2', vibana_own_analysis='anharm')
+
+    from wilson_suite.wilson_utils.paths import SUITE_ROOT
+    calc_setup = ws.main.abstractions.DataOriginInfo(source_type='gaussian',
+                                                     lvl_theory='HF', 
+                                                     basis_set='STO-3G', 
+                                                     base_file_loc=SUITE_ROOT+'/../data_for_tests/g16_h2o_HF_STO3G.out')
+    
+    from wilson_suite.wilson_utils.wilson_data_obtainer import wilson_data_obtainer
+    ws.main.main_functions.get_data_for_vibanalysers(vib_ana=vib_ana, 
+                                                     calc_setup=calc_setup, 
+                                                     obtainer=wilson_data_obtainer)
