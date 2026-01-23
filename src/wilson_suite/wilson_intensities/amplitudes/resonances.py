7713db781a30524be96d866a1bc2fdb988a5c517
Vanda Le <vandwlev@gmail.com>
2026-01-23 10:05:37 +0100

    rm print

--- src/wilson_suite/wilson_intensities/amplitudes/resonances.py
+++ src/wilson_suite/wilson_intensities/amplitudes/resonances.py
@@ -117,10 +117,6 @@ def solve_LSE_motif(motif: ResonanceMotif,
     A = np.array(coeff_matrix)
     b = np.array(constants)
 
-    # print('motif', motif)
-    # print('coeff_matrix\n', A)
-    # print('constants\n', b)
-
     try:
         solution = np.linalg.solve(A, b)
         from wilson_suite.wilson_utils.common_labels import num_cap_alpha_labels
