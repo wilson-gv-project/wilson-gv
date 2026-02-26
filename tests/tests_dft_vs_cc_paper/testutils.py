d9eceb07d92aaa549e89eb762e5426583b271f7b
Vanda Le <vandwlev@gmail.com>
2026-02-26 21:14:59 +0100

    upd test data fixtures

--- tests/tests_dft_vs_cc_paper/testutils.py
+++ tests/tests_dft_vs_cc_paper/testutils.py
@@ -1,4 +1,5 @@
 from wilson_suite.wilson_intensities.amplitudes.spectrum_composition import SpectralFeature, ResLocGeoObject, SpectralWindow
+from wilson_suite.wilson_intensities.amplitudes.term_parts import TermParametersChoice, ResonanceMotif, ResonanceCondition, ParameterSet
 
 class MakeObjects:
     
@@ -7,7 +8,6 @@ class MakeObjects:
         """
         with random term_contributions for now
         """
-        from wilson_suite.wilson_intensities.amplitudes.term_parts import TermParametersChoice, ResonanceMotif, ResonanceCondition, ParameterSet
         # if 
         rcs = [ResonanceCondition.make_from_tuples(left_state=(), right_state=('a',), pert_freqs=('-A',)),
                ResonanceCondition.make_from_tuples(left_state=('a', 'b'), right_state=('b',), pert_freqs=('B',))]
@@ -34,15 +34,34 @@ class MakeObjects:
     
     @staticmethod
     def mk_features_non_ovrl() -> list[SpectralFeature]:
+
+        rcs1 = [ResonanceCondition.make_from_tuples(left_state=(), right_state=('a',), pert_freqs=('-A',)),
+               ResonanceCondition.make_from_tuples(left_state=('a', 'b'), right_state=('b',), pert_freqs=('B',))]
+        res_motif1 = ResonanceMotif(rcs1)
+
+        param_set1 = ParameterSet({'a': 0, 'b': 1})
+        term_contrib1 = TermParametersChoice(res_motif=res_motif1,
+                                            states_parameters=(param_set1,),
+                                            term_ids=tuple([hash(res_motif1)]))
+
+        rcs2 = [ResonanceCondition.make_from_tuples(left_state=(), right_state=('a',), pert_freqs=('-A',)),
+               ResonanceCondition.make_from_tuples(left_state=('b',), right_state=('a',), pert_freqs=('B',))]
+        res_motif2 = ResonanceMotif(rcs2)
+
+        param_set2 = ParameterSet({'a': 1, 'b': 2})
+        term_contrib2 = TermParametersChoice(res_motif=res_motif2,
+                                            states_parameters=(param_set2,),
+                                            term_ids=tuple([hash(res_motif2)]))
         
         feat1 = SpectralFeature(location=ResLocGeoObject({'A': 1119.5, 'B': 2921.}), 
                                 lineshape_parameter=4.5, 
                                 amplitude_coeff=-4.32e-06, 
-                                term_contributions=('smth',))
-        feat2 = SpectralFeature(location=ResLocGeoObject({'A': 2219.5, 'B': 3177.}), 
+                                term_contributions=(term_contrib1,))
+        feat2 = SpectralFeature(location=ResLocGeoObject({'A': 964., 'B': 270.}), 
                                 lineshape_parameter=4.5, 
                                 amplitude_coeff=-1.12e-05, 
-                                term_contributions=('smth',))
+                                term_contributions=(term_contrib2,))
+        
         features = SpectralFeature.dress_these_with_boxes([feat1, feat2],
                                                           max_intensity=feat2.get_intensity()+1., 
                                                           min_intensity=feat1.get_intensity()-1.0e3,
@@ -54,15 +73,33 @@ class MakeObjects:
 
     @staticmethod
     def mk_features_ovrl() -> list[SpectralFeature]:
+
+        rcs1 = [ResonanceCondition.make_from_tuples(left_state=(), right_state=('a',), pert_freqs=('-A',)),
+               ResonanceCondition.make_from_tuples(left_state=('a', 'b'), right_state=('b',), pert_freqs=('B',))]
+        res_motif1 = ResonanceMotif(rcs1)
+
+        param_set1 = ParameterSet({'a': 0, 'b': 1})
+        term_contrib1 = TermParametersChoice(res_motif=res_motif1,
+                                            states_parameters=(param_set1,),
+                                            term_ids=tuple([hash(res_motif1)]))
+
+        rcs2 = [ResonanceCondition.make_from_tuples(left_state=(), right_state=('a',), pert_freqs=('-A',)),
+                ResonanceCondition.make_from_tuples(left_state=('b',), right_state=('a',), pert_freqs=('B',))]
+        res_motif2 = ResonanceMotif(rcs2)
+
+        param_set2 = ParameterSet({'a': 0, 'b': 1})
+        term_contrib2 = TermParametersChoice(res_motif=res_motif2,
+                                            states_parameters=(param_set2,),
+                                            term_ids=tuple([hash(res_motif2)]))
         
-        feat1 = SpectralFeature(location=ResLocGeoObject({'A': 1189.5, 'B': 2988.}), 
+        feat1 = SpectralFeature(location=ResLocGeoObject({'A': 1000., 'B': 520.}), 
                                 lineshape_parameter=4.5, 
                                 amplitude_coeff=-4.32e-06, 
-                                term_contributions=('smth',))
-        feat2 = SpectralFeature(location=ResLocGeoObject({'A': 1209.5, 'B': 3017.}), 
+                                term_contributions=(term_contrib1,))
+        feat2 = SpectralFeature(location=ResLocGeoObject({'A': 1000., 'B': 500.}), 
                                 lineshape_parameter=4.5, 
                                 amplitude_coeff=-1.12e-05, 
-                                term_contributions=('smth',))
+                                term_contributions=(term_contrib2,))
         features = SpectralFeature.dress_these_with_boxes([feat1, feat2],
                                                           max_intensity=feat2.get_intensity()+1., 
                                                           min_intensity=feat1.get_intensity()-1.0e3,
@@ -70,6 +107,7 @@ class MakeObjects:
                                                           scale_wrt_max_intensity=True,
                                                           minimum_box_padding=30.0,
                                                           )
+        assert features[0].feat_box.overlaps(features[1].feat_box)
         return features
     
     @staticmethod
@@ -95,8 +133,14 @@ class MakeObjects:
         return GridRegion(domain=domain,
                           coords=coords)
     
+
     @staticmethod
-    def mk_vibstates_states():
+    def mk_gridregion_for_feature(feature, coords):
+        return
+
+
+    @staticmethod
+    def mk_vibstates_states(closer=False):
         """
         rcs = [ResonanceCondition.make_from_tuples(left_state=('',), right_state=('a',), pert_freqs=('-A',)),
                ResonanceCondition.make_from_tuples(left_state=('a', 'b'), right_state=('b',), pert_freqs=('-B',))]
@@ -109,28 +153,34 @@ class MakeObjects:
         2921 = 0,1-1
         """
         from wilson_suite.wilson_main import abstractions as wm_abst
-        
-        states = (
-            wm_abst.VibState(harm_quanta_coeffs={(0,):1.}, state_label='0', energy=1119.5, harmonic_WF=True),
-            wm_abst.VibState(harm_quanta_coeffs={(1,):1.}, state_label='1', energy=964., harmonic_WF=True),
-            wm_abst.VibState(harm_quanta_coeffs={(2,):1.}, state_label='2', energy=1234., harmonic_WF=True),
-            wm_abst.VibState(harm_quanta_coeffs={(0, 0):1.}, state_label='0,0', energy=1864., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(0, 1):1.}, state_label='0,1', energy=3885., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(1, 1):1.}, state_label='1,1', energy=2368., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(1, 2):1.}, state_label='1,2', energy=2360., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(2, 2):1.}, state_label='2,2', energy=2362., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(0, 2):1.}, state_label='0,2', energy=2274., harmonic_WF=False),
+        if not closer:
+            states = (
+                wm_abst.VibState(harm_quanta_coeffs={(0,):1.}, state_label='0', energy=1119.5, harmonic_WF=True),
+                wm_abst.VibState(harm_quanta_coeffs={(1,):1.}, state_label='1', energy=964., harmonic_WF=True),
+                wm_abst.VibState(harm_quanta_coeffs={(2,):1.}, state_label='2', energy=1234., harmonic_WF=True),
+                wm_abst.VibState(harm_quanta_coeffs={(0, 0):1.}, state_label='0,0', energy=1864., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(0, 1):1.}, state_label='0,1', energy=3885., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(1, 1):1.}, state_label='1,1', energy=2368., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(1, 2):1.}, state_label='1,2', energy=2360., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(2, 2):1.}, state_label='2,2', energy=2362., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(0, 2):1.}, state_label='0,2', energy=2274., harmonic_WF=False),
 
-            wm_abst.VibState(harm_quanta_coeffs={(0, 0, 0):1.}, state_label='0,0,0', energy=2685., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(1, 1, 1):1.}, state_label='1,1,1', energy=3581., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(2, 2, 2):1.}, state_label='2,2,2', energy=3690., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(0, 1, 2):1.}, state_label='0,1,2', energy=3742., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(0, 1, 1):1.}, state_label='0,1,1', energy=3318., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(0, 2, 2):1.}, state_label='0,2,2', energy=3680., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(0, 0, 1):1.}, state_label='0,0,1', energy=3155., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(0, 0, 2):1.}, state_label='0,0,2', energy=3498., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(1, 1, 2):1.}, state_label='1,1,2', energy=3594., harmonic_WF=False),
-            wm_abst.VibState(harm_quanta_coeffs={(1, 2, 2):1.}, state_label='1,2,2', energy=3642., harmonic_WF=False),
-        )
+                wm_abst.VibState(harm_quanta_coeffs={(0, 0, 0):1.}, state_label='0,0,0', energy=2685., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(1, 1, 1):1.}, state_label='1,1,1', energy=3581., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(2, 2, 2):1.}, state_label='2,2,2', energy=3690., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(0, 1, 2):1.}, state_label='0,1,2', energy=3742., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(0, 1, 1):1.}, state_label='0,1,1', energy=3318., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(0, 2, 2):1.}, state_label='0,2,2', energy=3680., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(0, 0, 1):1.}, state_label='0,0,1', energy=3155., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(0, 0, 2):1.}, state_label='0,0,2', energy=3498., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(1, 1, 2):1.}, state_label='1,1,2', energy=3594., harmonic_WF=False),
+                wm_abst.VibState(harm_quanta_coeffs={(1, 2, 2):1.}, state_label='1,2,2', energy=3642., harmonic_WF=False),
+            )
+        else:
+            states = (
+                wm_abst.VibState(harm_quanta_coeffs={(0,):1.}, state_label='0', energy=1000., harmonic_WF=True),
+                wm_abst.VibState(harm_quanta_coeffs={(1,):1.}, state_label='1', energy=1500., harmonic_WF=True),
+                wm_abst.VibState(harm_quanta_coeffs={(0, 1):1.}, state_label='0,1', energy=2020., harmonic_WF=False),
+            )
 
         return states
