202019cc8de15470a87917e5c5a582879a1e6324
ringholm <mringholm@gmail.com>
2026-06-04 12:31:52 +0200

    Added test for get_dressed_vib_sos_with_exp_filtering for EVV

--- src/wilson_suite/wilson_derive/tests/test_vib_rsp_sos.py
+++ src/wilson_suite/wilson_derive/tests/test_vib_rsp_sos.py
@@ -3,8 +3,6 @@ from fractions import Fraction
 import pytest
 
 from wilson_suite.wilson_derive.vib_rsp_sos import get_vib_sos
-from wilson_suite.wilson_derive.abstractions import QOperator, VibStateSymbolic
-from wilson_suite.wilson_utils import common_labels as wu_common
 
 def test_get_vib_sos():
 
