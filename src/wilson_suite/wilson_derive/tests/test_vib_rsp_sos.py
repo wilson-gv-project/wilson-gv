206468cd54a90783737b1634de704a1a3b41f257
ringholm <mringholm@gmail.com>
2026-06-05 12:01:25 +0200

    Revert "Added test_make_anharm_orders, rm vib_rsp_sos tests for outdated fns"
    This reverts commit bf8745726251ee168a3f2c3f98306081d4ab36df.

--- src/wilson_suite/wilson_derive/tests/test_vib_rsp_sos.py
+++ src/wilson_suite/wilson_derive/tests/test_vib_rsp_sos.py
@@ -9,3 +9,16 @@ def test_get_vib_sos():
     order = 6
 
     R_sos_new = get_vib_sos(order)
+
+
+def test_make_op_sel_set():
+
+    pass
+
+def get_op_combs_rec_t():
+
+    pass
+
+def get_vib_contribs_abstract():
+
+    pass
