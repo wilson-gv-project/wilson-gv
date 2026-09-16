5edd3afa5aef1984babd705960e68533fb5b3759
Vanda Le <vandwlev@gmail.com>
2026-09-16 13:06:38 +0200

    fixing tests for make_SpectralAxisSet

--- src/wilson_suite/wilson_utils/tests/test_builders.py
+++ src/wilson_suite/wilson_utils/tests/test_builders.py
@@ -1,5 +1,6 @@
 from ..builders import show_valid_axis_combs, show_term_latex, make_SpectralAxisSet
 import wilson_suite as ws
+from wilson_suite.wilson_experiment.indep_vars_and_axes import SpectralAxis, SpectralAxisChoices, SpectralAxisSet, IndependentVariableSet, SignedPulseTuple
 
 evv_exp = ws.fixtures.evv_experiment()
 terms = ws.derive.derive.get_fully_enhanced_terms(experiment=evv_exp)
@@ -17,8 +18,48 @@ def test_show_valid_axis_combs():
     show_valid_axis_combs(evv_exp.valid_axis_combs)
 
 def test_make_SpectralAxisSet():
+    """
+    1 SignedPulseTuple instance per indep variable - 
+        making several SignedPulseTuples per IndependentVariableSet
+    """
+    # independent vars here are -1 and 2
     axis_choice = make_SpectralAxisSet({'A': [-1], 'B': [-1, 2]})
+
+    varsA = IndependentVariableSet((SignedPulseTuple((-1,)),))
+    axA = SpectralAxis('A', varsA)
+
+    varsB = IndependentVariableSet((SignedPulseTuple((-1,)),SignedPulseTuple((2,))))
+    axB = SpectralAxis('B', varsB)
+
+    refaxes = SpectralAxisSet((axA, axB))
+    assert axis_choice.axes == refaxes.axes
+
+    # -------------------------------------------------------------
+    # independent vars here are -1+2 and 3
+    axis_choice = make_SpectralAxisSet({'A': [(-1,2),], 'B': [(-1, 2), 3]})
     print(axis_choice)
+    varsA = IndependentVariableSet((SignedPulseTuple((-1,2)),))
+    axA = SpectralAxis('A', varsA)
+
+    varsB = IndependentVariableSet((SignedPulseTuple((-1,2)),SignedPulseTuple((3,))))
+    axB = SpectralAxis('B', varsB)
+
+    refaxes = SpectralAxisSet((axA, axB))
+    assert axis_choice.axes == refaxes.axes
+
+
+    # independent vars here are -1+2 and 3
+    axis_choice = make_SpectralAxisSet({'A': [(-1,2),], 'B': [3,]})
+    print(axis_choice)
+    varsA = IndependentVariableSet((SignedPulseTuple((-1,2)),))
+    axA = SpectralAxis('A', varsA)
+
+    varsB = IndependentVariableSet((SignedPulseTuple((3,)),))
+    axB = SpectralAxis('B', varsB)
+
+    refaxes = SpectralAxisSet((axA, axB))
+    assert axis_choice.axes == refaxes.axes
+
 
 def test_show_term_latex():
 
